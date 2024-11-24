0de34e7473ac389944623211ac4a6ed2c90c914f
Clemzy321 <clemzy321@gmail.com>
2024-11-24 21:47:31 +0100

    Changes 24.Nov

--- main.c
+++ main.c
@@ -26,10 +26,14 @@
 int AddIn_main(int isAppli, unsigned short OptionNum)
 {
     int getInput();
-    void setXY(int numpad[9], int numY[3], int XY[2]);
+    void nextXY(int numpad[9], int numY[3], int nXY[2]);
     void miniTTT(int xOffset, int yOffset);
     void drawShape(int xOffset, int yOffset, int shape[3][3]);
     void initDisp();
+    void drawOutline(int XY[2]);
+    void eraseOutline(int XY[2]);
+    void memorize(int memory[3][3][3][3], int XY1[2], int XY2[2], int player);
+    void nextMove(int memory[3][3][3][3], int nXY[2], int numpad[9], int numY[3], int XY[2]);
     
     unsigned int key;
     int c = 0;
@@ -39,10 +43,33 @@ int AddIn_main(int isAppli, unsigned short OptionNum)
     4, 5, 6,
     1, 2, 3};
     
+    int memory[3][3][3][3] = {
+    {{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+    
+    {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+    
+    {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}},
+    
+    {{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+   
+    {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+    
+    {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}},
+    
+    {{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+    
+    {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+    
+    {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}}
+    };
+    
+    
+    
     int numY[3] = {0, 1, 2};
     
-    int XY[2] = {0, 0};
-    int startXY[2] = {0, 0};
+    int XY1[2] = {0, 0};
+    int XY2[2] = {0, 0};
+    int nXY[2] = {0, 0};
 
     int circle[3][3] = {  
         {1, 1, 1},
@@ -55,29 +82,30 @@ int AddIn_main(int isAppli, unsigned short OptionNum)
         {0, 1, 0},
         {1, 0, 1}
     };
-    
+
     int* shape;
 
     Bdisp_AllClr_DDVRAM();
     initDisp();
     Bdisp_PutDisp_DD();
     
-    setXY(numpad, numY, XY);
-    
     while(1){
-        if (c%2){
-            shape = circle;
-        }
-        else{
-            shape = cross;
-        }
-    
-        startXY[0] = XY[0];
-        startXY[1] = XY[1];
+        //if (c%2){
+            //shape = circle;
+        //}
+        //else{
+            //shape = cross;
+        //}
         
-        setXY(numpad, numY, XY);
-    
-        drawShape(XY[0]*6+4+startXY[0]*20, XY[1]*6+4+startXY[1]*20, shape);   
+        nextMove(memory, nXY, numpad, numY, XY2);
+        XY1[0] = XY2[0];
+        XY1[1] = XY2[1];
+        XY2[0] = nXY[0];
+        XY2[1] = nXY[1];
+        
+        memorize(memory, XY1, XY2, (c%2)+1);
+        
+        drawShape(XY2[0]*6+4+XY1[0]*20, XY2[1]*6+4+XY1[1]*20, circle);   
         Bdisp_PutDisp_DD();
         
         c++;
@@ -98,7 +126,7 @@ int getInput(){
     return(key-48);
 }
 
-void setXY(int numpad[9], int numY[3], int XY[2]){
+void nextXY(int numpad[9], int numY[3], int XY[2]){
     int inp = getInput(); 
     inp = numpad[inp-1];
     
@@ -123,6 +151,30 @@ void drawShape(int xOffset, int yOffset, int shape[3][3]){
         for (x=0; x<3; x++){  
             Bdisp_SetPoint_VRAM(x+xOffset, y+yOffset, shape[y][x]);
         }
+    }
+}
+
+void drawOutline(int XY[2]){
+    Bdisp_DrawLineVRAM(2+XY[0]*20, 2+XY[1]*20, 2+XY[0]*20, 20+XY[1]*20);
+    Bdisp_DrawLineVRAM(2+XY[0]*20, 20+XY[1]*20, 20+XY[0]*20, 20+XY[1]*20);
+    Bdisp_DrawLineVRAM(20+XY[0]*20, 20+XY[1]*20, 20+XY[0]*20, 2+XY[1]*20);
+    Bdisp_DrawLineVRAM(20+XY[0]*20, 2+XY[1]*20, 2+XY[0]*20, 2+XY[1]*20);
+}
+
+void eraseOutline(int XY[2]){
+    Bdisp_ClearLineVRAM(2+XY[0]*20, 2+XY[1]*20, 2+XY[0]*20, 20+XY[1]*20);
+    Bdisp_ClearLineVRAM(2+XY[0]*20, 20+XY[1]*20, 20+XY[0]*20, 20+XY[1]*20);
+    Bdisp_ClearLineVRAM(20+XY[0]*20, 20+XY[1]*20, 20+XY[0]*20, 2+XY[1]*20);
+    Bdisp_ClearLineVRAM(20+XY[0]*20, 2+XY[1]*20, 2+XY[0]*20, 2+XY[1]*20);
+}
+
+void memorize(int memory[3][3][3][3], int XY1[2], int XY2[2], int player){
+    memory[XY1[1]][XY1[0]][XY2[1]][XY2[0]] = player;
+} 
+
+void nextMove(int memory[3][3][3][3], int nXY[2], int numpad[9], int numY[3], int XY[2]){
+    while (memory[XY[1]][XY[0]][nXY[1]][nXY[0]] != 0){
+        nextXY(numpad, numY, XY);
     }
 }
 
